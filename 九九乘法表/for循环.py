93d9298f67e177b749b54757c4033e71192ba051
江有小老虎 <3056113383@QQ.com>
2024-08-06 12:25:23 +0800

    Add files via upload

---
+++ 九九乘法表/for循环.py
@@ -0,0 +1,6 @@
+#for循环打印99乘法表
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print(f"{j}*{i}={j * i}\t", end='')
+    print()
+
---
+++ 九九乘法表/while循环.py
@@ -0,0 +1,9 @@
+#while循环打印99乘法表
+i = 1
+while i <= 9:
+    j = 1
+    while j <= i:
+        print(f"{j}*{i}={j * i}\t", end='')
+        j += 1
+    i += 1
+    print()
